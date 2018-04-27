e9f9e3c4d863fd0ffa745b79eb7740b189d38716
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-04-27 09:21:21 -0400

    Be lenient with removing whitespace on post-processing

--- pretty_html.js
+++ pretty_html.js
@@ -6,7 +6,7 @@ let htmlFilePath = argv.html;
 
 fs.readFile(htmlFilePath, function (err, data) {
     if (err) return console.error(err);
-    let htmlPrettyString = pretty(data.toString(), {ocd: true});
+    let htmlPrettyString = pretty(data.toString(), {ocd: false});
     fs.writeFile(htmlFilePath, htmlPrettyString, function (err) {
         if (err) return console.log(err);
     });
